fb9d1273cc595f559f65a7cbc01168259001c60d
shoaib-coder-786 <shoaib_co20b11_21@dtu.ac.in>
2023-07-16 11:32:53 +0530

    projec is ready for deployment

---
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -0,0 +1,37 @@
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+
+const AdvertWidget = () => {
+  const { palette } = useTheme();
+  const dark = palette.neutral.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
+  return (
+    <WidgetWrapper>
+      <FlexBetween>
+        <Typography color={dark} variant="h5" fontWeight="500">
+          Sponsored
+        </Typography>
+        <Typography color={medium}>Create Ad</Typography>
+      </FlexBetween>
+      <img
+        width="100%"
+        height="auto"
+        alt="advert"
+        src="/assets/info4.jpg"
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+      <FlexBetween>
+        <Typography color={main}>ChillMusic</Typography>
+        <Typography color={medium}>ListenMusic.com</Typography>
+      </FlexBetween>
+      <Typography color={medium} m="0.5rem 0">
+        Your pathway to get enjoy and relac with latest songs and albums of the year .
+      </Typography>
+    </WidgetWrapper>
+  );
+};
+
+export default AdvertWidget;
